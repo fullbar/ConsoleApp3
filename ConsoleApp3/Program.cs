7f08b72268c390fe27c9015af1eb197a529cd834
Andrey Georgiev <andreshko.georgiev@gmail.com>
2017-07-20 19:42:16 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int b;
+        string baseTestValue = Console.ReadLine();
+        b = int.Parse(baseTestValue);
+
+        int p;
+        string powerTextValue = Console.ReadLine();
+        p = int.Parse(powerTextValue);
+
+        Console.WriteLine(Math.Pow(b, p));
+    }
+}
+
